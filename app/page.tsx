aa0165addfd780a7d45a6f258e96cad66e1ab0da
mahmudulshawn <mahmudulalamshawn@gmail.com>
2026-03-19 21:23:12 +0600

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main>
-        Hello world
-      </main>
+    <div className="">
+      Home
     </div>
   );
-}
+};
+
+export default Home;
